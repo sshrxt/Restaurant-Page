dc97311eded021df9a27706ca2d536cc8c957046
Shrut <shrutpatel20@gmail.com>
2024-06-06 13:39:38 -0500

    change menu card images

--- src/index.js
+++ src/index.js
@@ -1,35 +1,42 @@
-import './style.css';
-import { HomePage } from './home';
-import { MenuPage } from './menu-page';
-import Koala from './assets/koala.jpg'
+import "./style.css";
+import { HomePage } from "./home";
+import { MenuPage } from "./menu-page";
+import Koala from "./assets/koala.jpg";
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    const content = document.getElementById('content');
+document.addEventListener("DOMContentLoaded", () => {
+  const content = document.getElementById("content");
 
-    
-    
-    // const homeBtn = document.getElementById('home');
-    // homeBtn.addEventListener("click", ()=> {
-    //     while (content.firstChild) {
-    //         content.removeChild(content.firstChild);
-    //     }
-    //     const homePage = HomePage();
-    //     content.appendChild(homePage);
-    //     const menuButton = document.querySelector('.menu-button');
-    //     menuButton.addEventListener("click", ()=>{
-    //         while (content.firstChild) {
-    //             content.removeChild(content.firstChild);
-    //         }
-    //         content.appendChild(MenuPage());
-    //     });
-    // });
+//   content.appendChild(HomePage());
 
-    // const menuBtn = document.getElementById('menu');
-    // menuBtn.addEventListener("click", ()=> {
-    //     while (content.firstChild) {
-    //         content.removeChild(content.firstChild);
-    //     }
-    //     content.appendChild(MenuPage());
-    // });
-    
+//   const menuButton = document.querySelector(".menu-button");
+//   menuButton.addEventListener("click", () => {
+//     while (content.firstChild) {
+//       content.removeChild(content.firstChild);
+//     }
+//     content.appendChild(MenuPage());
+//   });
+
+//   const homeBtn = document.getElementById("home");
+//   homeBtn.addEventListener("click", () => {
+//     while (content.firstChild) {
+//       content.removeChild(content.firstChild);
+//     }
+//     const homePage = HomePage();
+//     content.appendChild(homePage);
+//     const menuButton = document.querySelector(".menu-button");
+//     menuButton.addEventListener("click", () => {
+//       while (content.firstChild) {
+//         content.removeChild(content.firstChild);
+//       }
+//       content.appendChild(MenuPage());
+//     });
+//   });
+
+//   const menuBtn = document.getElementById("menu");
+//   menuBtn.addEventListener("click", () => {
+//     while (content.firstChild) {
+//       content.removeChild(content.firstChild);
+//     }
+//     content.appendChild(MenuPage());
+//   });
 });
--- src/menu-items.js
+++ src/menu-items.js
@@ -1,46 +1,52 @@
-import ChickenQuesadilla from './assets/koala.jpg'
-
+import berrySmoothie from './assets/berry.jpg'
+import chipotleBowl from './assets/bowl.jpg'
+import hummusWrap from './assets/hummus.jpg'
+import nachos from './assets/nachos.jpg'
+import pitaPizza from './assets/pita.jpg'
+import tiramisu from './assets/tiramisu.jpg'
+import plov from './assets/plov.jpg'
+import panini from './assets/panini.jpg'
 
 
 export const menuItems = [
     {
-        title: "Chicken Quesadilla",
-        description: "A crispy and nice delight good for every thing lol!",
-        image: ChickenQuesadilla
+        title: "Berry Bomb Smoothie",
+        description: "A flavor which replicates a bomb of berries dropping into one's mouth",
+        image: berrySmoothie
     },
     {
-        title: "Chicken Quesadilla",
-        description: "A crispy and nice delight good for every thing lol!",
-        image: ChickenQuesadilla
+        title: "Chipotle-style Bowl",
+        description: "Inspired by Chipotle, but better",
+        image: chipotleBowl
     },
     {
-        title: "Hey",
-        description: "A crispy and nice delight good for every thing lol!",
-        image: ChickenQuesadilla
+        title: "Hummus Pita Wrap",
+        description: "A genie's wish",
+        image: hummusWrap
     },
     {
-        title: "Chicken Quesadilla",
-        description: "A crispy and nice delight good for every thing lol!",
-        image: ChickenQuesadilla
+        title: "Plov",
+        description: "My pookie's favorite food, on the house",
+        image: plov
     },
     {
-        title: "Chicken Quesadilla",
-        description: "A crispy and nice delight good for every thing lol!",
-        image: ChickenQuesadilla
+        title: "Shrut's House Nachos",
+        description: "A crispy and satisting dish filled with nacho cheese",
+        image: nachos
     },
     {
-        title: "Chicken Quesadilla",
-        description: "A crispy and nice delight good for every thing lol!",
-        image: ChickenQuesadilla
+        title: "Low Calorie Pizza",
+        description: "Pizza, but healthy?",
+        image: pitaPizza
     },
     {
-        title: "Chicken Quesadilla",
-        description: "A crispy and nice delight good for every thing lol!",
-        image: ChickenQuesadilla
+        title: "Tiramisu",
+        description: "A must have dessert, imported from italy",
+        image: tiramisu
     },
     {
-        title: "Chicken Quesadilla",
-        description: "A crispy and nice delight good for every thing lol!",
-        image: ChickenQuesadilla
+        title: "Veggie Panini",
+        description: "For vegetarian folks",
+        image: panini
     },
 ];
